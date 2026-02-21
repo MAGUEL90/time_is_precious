e251af2f87ff9dd93c3d9899e7e7d945dddefa85
Hendro Chandra <156631787+MAGUEL90@users.noreply.github.com>
2026-02-21 07:02:41 +0700

    feat(ui)
    add inventory UI layout and item slot

--- scenes/time_label/time_label.gd
+++ scenes/time_label/time_label.gd
@@ -1,6 +1,9 @@
 extends Control
 
-@onready var label: Label = $VBoxContainer/Label
+@onready var day_label: Label = $VBoxContainer/HBoxContainer/DayLabel
+@onready var time_label: Label = $VBoxContainer/HBoxContainer/TimeLabel
+@onready var weather_label: Label = $VBoxContainer/WeatherLabel
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,8 +12,9 @@ func _ready() -> void:
 			int(TimeComponentManager.current_minute), TimeComponentManager.current_weather)
 
 func _on_time_changed(day:int, hour:int, minute:int, weather: String) -> void:
-	label.text = "Day: %02d  Hour: %02d  Minute: %02d \n Weather: %s" \
-			% [day, hour, minute, weather]
+	day_label.text = "Day: %02d " % [day]
+	time_label.text = "| Hour: %02d Minute: %02d " % [hour, minute]
+	weather_label.text = "Weather: %s" % [weather]
 
 	
 	
