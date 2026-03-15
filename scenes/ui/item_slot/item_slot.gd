a837bc2c3dafcfd17b902c23a781f4550577aa02
Hendro <hendrogokielz90@gmail.com>
2026-03-16 05:49:23 +0700

    fix(item-slot): renaming item slot icon parameter

--- scenes/ui/item_slot/item_slot.gd
+++ scenes/ui/item_slot/item_slot.gd
@@ -3,7 +3,7 @@ extends Button
 @onready var asset_icon: TextureRect = $MarginContainer/AssetIcon
 @onready var asset_qty: Label = $MarginContainer/AssetQty
 
-func set_item(item_id: String, quantity: int, icon: Texture2D) -> void:
-	asset_icon.texture = icon
+func set_item(item_id: String, quantity: int, _icon: Texture2D) -> void:
+	asset_icon.texture = _icon
 	asset_qty.text = str(max(quantity, 0))
 	tooltip_text = "%s x%s" % [item_id.replace("_", " ").capitalize(), asset_qty.text]
