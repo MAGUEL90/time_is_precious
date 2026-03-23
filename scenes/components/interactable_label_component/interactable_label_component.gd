7253cef8fcdc7452c98ac22c7b7c97d83dad5b74
Hendro <hendrogokielz90@gmail.com>
2026-03-23 22:07:39 +0700

    feat(interactable-label-component): setup default position over every node and adding set_text func

--- scenes/components/interactable_label_component/interactable_label_component.gd
+++ scenes/components/interactable_label_component/interactable_label_component.gd
@@ -1,1 +1,10 @@
 class_name InteractableLabelComponent extends Control
+
+@export var position_offset: Vector2 = Vector2(-8, -20)
+@onready var label: Label = $VBoxContainer/Label
+
+func _ready() -> void:
+	position = position_offset
+
+func set_text(new_text: String) -> void:
+	label.text = new_text
