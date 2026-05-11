03e31e5f8075cb7a7f157d5e97487af5fb8d905b
Hendro <hendrogokielz90@gmail.com>
2026-05-12 06:35:25 +0700

    feat(jobs): add mudbrick job resource

--- resources/job_data/job_data.gd
+++ resources/job_data/job_data.gd
@@ -1,11 +1,10 @@
 class_name JobData extends Resource
 
-var job_id: String = ""
-var display_name: String = ""
-var inputs: Dictionary[String, int] = {}
-var outputs: Dictionary[String, int] = {}
-var required_tool_id: String = "" # Kosong jika tidak ada standard
-
+@export var job_id: String = ""
+@export var display_name: String = ""
+@export var inputs: Dictionary[String, int] = {}
+@export var outputs: Dictionary[String, int] = {}
+@export var required_tool_id: String = "" # Kosong jika tidak ada standard
 @export var base_duration_minutes: int = 0
 @export var tool_durability_loss: int = 0
 @export var waste_rate: float = 0.1
