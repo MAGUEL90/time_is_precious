da9c51d8bc8aae0466577de4d9a973df18526aa6
Hendro <hendrogokielz90@gmail.com>
2026-06-14 05:37:08 +0700

    Prototype minimal worker loop

--- scripts/autoload/citizen_needs_manager/citizen_needs_manager.gd
+++ scripts/autoload/citizen_needs_manager/citizen_needs_manager.gd
@@ -2,6 +2,8 @@ extends Node
 
 const FOOD_SUPPLY_PER_CITIZEN_PER_DAY: int = 1
 const CLOTHING_SUPPLY_PER_CITIZEN_PER_DAY: int = 1
+const FOOD_SUPPLY_PER_WORKER_PER_DAY: int = 1
+const CLOTHING_SUPPLY_PER_WORKER_PER_DAY: int = 1
 
 var last_food_fulfilled_count: int = 0
 var last_food_unfulfilled_count: int = 0
@@ -31,6 +33,34 @@ func process_daily_needs() -> void:
 		else:
 			citizen.satisfaction = clampf(citizen.satisfaction - 0.10, 0.01, 0.99)
 			citizen.reliability = clampf(citizen.reliability - 0.05, 0.01, 0.99)
+
+	process_daily_worker_needs()
+
+func process_daily_worker_needs() -> void:
+	# Worker diproses setelah citizen agar citizen tetap dapat prioritas stok.
+	# Sisa shelter dihitung dari kapasitas dikurangi yang sudah terpakai citizen.
+	var remaining_shelter_capacity: int = CityStockManager.shelter_capacity - last_shelter_capacity_fulfilled_count
+
+	for worker in WorkerDatabase.get_all_workers():
+		if not (worker is WorkerData):
+			continue
+
+		var worker_data: WorkerData = worker as WorkerData
+		worker_data.food_fulfilled = CityStockManager.consume_food_supply(FOOD_SUPPLY_PER_WORKER_PER_DAY)
+		worker_data.clothing_fulfilled = CityStockManager.consume_clothing_supply(CLOTHING_SUPPLY_PER_WORKER_PER_DAY)
+
+		if remaining_shelter_capacity > 0:
+			remaining_shelter_capacity -= 1
+			worker_data.shelter_fulfilled = true
+		else:
+			worker_data.shelter_fulfilled = false
+
+		if worker_data.are_basic_needs_fulfilled():
+			worker_data.satisfaction = clampf(worker_data.satisfaction + 0.05, 0.01, 0.99)
+			worker_data.reliability = clampf(worker_data.reliability + 0.03, 0.01, 0.99)
+		else:
+			worker_data.satisfaction = clampf(worker_data.satisfaction - 0.10, 0.01, 0.99)
+			worker_data.reliability = clampf(worker_data.reliability - 0.05, 0.01, 0.99)
 
 func process_daily_food_needs() -> void:
 	last_food_fulfilled_count = 0
